2b06bc02dbc8ae05b3cba0deae327e01e9e38cb0
Yaroslav9728 <31794068+Yaroslav9728@users.noreply.github.com>
2022-01-28 23:57:45 -0500

    Fixed version

--- FVApp/FVApp/Models/FutureValueModel.cs
+++ FVApp/FVApp/Models/FutureValueModel.cs
@@ -5,7 +5,7 @@ namespace FVApp.Models
     public class FutureValueModel
     {
         [Required(ErrorMessage = "Should be valid present value")]
-        public double? PresentValue { get; set; }
+        public decimal? PresentValue { get; set; }
 
         [Required(ErrorMessage = "Should be year")]
         [Range(1,10, ErrorMessage = "Can not be zero or greater than 10")]
@@ -13,21 +13,21 @@ namespace FVApp.Models
 
         [Required(ErrorMessage = "Should be valid interest rate")]
         [Range (0,100,ErrorMessage = "Can not be negative or greater than 100")]
-        public double? InterestRate { get; set; }
+        public decimal? InterestRate { get; set; }
 
-        public double CalculateFV()
+        public decimal CalculateFV()
         {
             if (InterestRate.HasValue && PresentValue.HasValue && Period.HasValue)
             {
-                double result = 1;
+                
 
-                double value = (PresentValue.Value * (1.0 + InterestRate.Value));
+                decimal futureValue = 0m;
 
                 for (int i = 0; i < Period; i++)
                 {
-                    result *= value;
+                    futureValue = (futureValue + PresentValue.Value) * (1 + InterestRate.Value);
                 }
-                return result;
+                return futureValue;
             }
             else
             {
