f66cd66550d5700ee79feedb0ff61dd96aaa85f8
Yaroslav9728 <31794068+Yaroslav9728@users.noreply.github.com>
2022-01-28 23:53:46 -0500

    Future Value Application

---
+++ FVApp/FVApp/Controllers/HomeController.cs
@@ -0,0 +1,53 @@
+﻿using FVApp.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FVApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IActionResult Index()
+        {
+            ViewBag.FV = 0;
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Index(FutureValueModel futureValueModel)
+        {
+            if(ModelState.IsValid)
+            {
+                ViewBag.FV = futureValueModel.CalculateFV();
+            }
+            else
+            {
+                ViewBag.FV = 0;
+            }
+            return View(futureValueModel);
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ FVApp/FVApp/Models/FutureValueModel.cs
@@ -0,0 +1,39 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FVApp.Models
+{
+    public class FutureValueModel
+    {
+        [Required(ErrorMessage = "Should be valid present value")]
+        public double? PresentValue { get; set; }
+
+        [Required(ErrorMessage = "Should be year")]
+        [Range(1,10, ErrorMessage = "Can not be zero or greater than 10")]
+        public int? Period { get; set; }
+
+        [Required(ErrorMessage = "Should be valid interest rate")]
+        [Range (0,100,ErrorMessage = "Can not be negative or greater than 100")]
+        public double? InterestRate { get; set; }
+
+        public double CalculateFV()
+        {
+            if (InterestRate.HasValue && PresentValue.HasValue && Period.HasValue)
+            {
+                double result = 1;
+
+                double value = (PresentValue.Value * (1.0 + InterestRate.Value));
+
+                for (int i = 0; i < Period; i++)
+                {
+                    result *= value;
+                }
+                return result;
+            }
+            else
+            {
+                return 0;
+            }
+        }
+
+    }
+}
